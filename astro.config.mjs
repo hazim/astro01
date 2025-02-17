a46bf3d5b20a1624ba2e0deb8c74aacdfc1bd8ab
Hazim Sami (he/him) <1193645+hazim@users.noreply.github.com>
2025-02-17 20:11:56 +0100

    update config for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: "shiki", // Enables syntax highlighting
+    shikiConfig: {
+      theme: "dracula", // Try other themes like "github-dark" or "nord"
+    },
+  },
   site: "https://frontendflux.netlify.app",
   integrations: [preact()]
 });
